e8ea9a19cd5a032887128dc0abc18bf627d979e7
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-05-02 12:26:31 +0100

    JDK7 does not require to be updated

--- 9000/update.sh
+++ 9000/update.sh
@@ -7,11 +7,12 @@
 	SHA="$(curl -SLfs https://repo1.maven.org/maven2/org/jruby/jruby-dist/${JRUBY_VERSION}/jruby-dist-${JRUBY_VERSION}-bin.tar.gz.sha256)"
 	SHA=$(printf '%s' ${SHA})
 
-	for i in $(ls); do
-		if [ -d "$i" ]; then
-			echo setting version on "$i/Dockerfile"
-			sed -i "s/ENV\ JRUBY_VERSION.*/ENV JRUBY_VERSION ${JRUBY_VERSION}/" "$i/Dockerfile"
-			sed -i "s/ENV\ JRUBY_SHA256.*/ENV JRUBY_SHA256 ${SHA}/" "$i/Dockerfile"
+	for i in $(find . -name 'Dockerfile'); do
+		# exclude JDK 7 as it does require 9.1
+		if ! [[ $i =~ .*/7-.* ]] ; then
+			echo setting version on "$i"
+			sed -i "s/ENV\ JRUBY_VERSION.*/ENV JRUBY_VERSION ${JRUBY_VERSION}/" "$i"
+			sed -i "s/ENV\ JRUBY_SHA256.*/ENV JRUBY_SHA256 ${SHA}/" "$i"
 		fi
 	done
 
