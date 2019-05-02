37928a29a71ed2a674cf47152793d3f017c467ab
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-05-02 12:46:12 +0100

    Support 'openjdk version 1.8.0_212' and 'openjdk version 12.0.1'

--- tests/test.sh
+++ tests/test.sh
@@ -4,7 +4,7 @@ version=${2?version is required}
 
 echo  "Test $1"
 test_name=" Test java -version '$version'"
-docker run -it --rm $image java -version | grep -q "openjdk version \"$version" && echo "${test_name} ... PASSED" || echo "${test_name} ... FAILED"
+docker run -it --rm $image java -version | grep -q "openjdk version \"$version\|1.$version" && echo "${test_name} ... PASSED" || echo "${test_name} ... FAILED"
 
 test_name=" Test Hello World"
 docker run -it --rm $image jruby -e "puts 'Hello World" | grep -q 'Hello World' && echo "${test_name} ... PASSED" || echo "${test_name} ... FAILED"
