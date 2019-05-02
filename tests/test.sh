1bd755f1ac2a7172b1fefb5f40c035ce52e94b2a
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-05-02 11:44:15 +0100

    Rename

--- tests/test.sh
+++ tests/test.sh
@@ -1,2 +1,10 @@
-echo  "Test $1\c"
-docker run -it --rm --name my-running-script $1 jruby -e "puts 'Hello World" | grep -q 'Hello World' && echo ' ...passed' || echo ' ...failed'
+#!/usr/bin/env bash
+image=${1?image is required}
+version=${2?version is required}
+
+echo  "Test $1"
+test_name=" Test java -version '$version'"
+docker run -it --rm $image java -version | grep -q "openjdk version \"$version" && echo "${test_name} ... PASSED" || echo "${test_name} ... FAILED"
+
+test_name=" Test Hello World"
+docker run -it --rm $image jruby -e "puts 'Hello World" | grep -q 'Hello World' && echo "${test_name} ... PASSED" || echo "${test_name} ... FAILED"
